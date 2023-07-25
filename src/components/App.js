ef697dfb8e392f4415759ade465e6a912515726b
anitha-anii <anithachowdary355@gmail.com>
2023-07-25 08:52:31 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -6,51 +6,69 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cartItems: [],
-      availableItems: [
+      cartItems: [
         { id: 1, name: 'Item 1', price: 10 },
-        { id: 2, name: 'Item 2', price: 20 },
-        { id: 3, name: 'Item 3', price: 15 },
+        { id: 2, name: 'Item 2', price: 15 },
+        { id: 3, name: 'Item 3', price: 20 },
       ],
+      newItemName: '',
+      newItemPrice: '',
     };
   }
 
-  addToCart = (item) => {
-    this.setState((prevState) => ({
-      cartItems: [...prevState.cartItems, item],
-    }));
-  };
-
-  removeItemFromCart = (itemId) => {
+  handleRemoveItem = (itemId) => {
     this.setState((prevState) => ({
       cartItems: prevState.cartItems.filter((item) => item.id !== itemId),
     }));
   };
 
+  handleAddItem = () => {
+    const { newItemName, newItemPrice } = this.state;
+    if (newItemName && newItemPrice) {
+      const newItem = {
+        id: Date.now(), // You can use any unique identifier for the new item
+        name: newItemName,
+        price: parseFloat(newItemPrice),
+      };
+
+      this.setState((prevState) => ({
+        cartItems: [...prevState.cartItems, newItem],
+        newItemName: '',
+        newItemPrice: '',
+      }));
+    }
+  };
+
+  handleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
+    const { newItemName, newItemPrice } = this.state;
+
     return (
       <div className="parent">
-        <h1>Shopping Cart</h1>
-        <div className="available-items">
-          <h2>Available Items</h2>
-          <ul>
-            {this.state.availableItems.map((item) => (
-              <li key={item.id}>
-                <span id="itemName">{item.name}</span> - <span id="itemPrice">${item.price}</span>
-                <button
-                  id="addToCartButton"
-                  onClick={() => this.addToCart(item)}
-                >
-                  Add to Cart
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <ChildComponent cartItems={this.state.cartItems} onRemoveItem={this.removeItemFromCart} />
+        <h2>Cart Items</h2>
+        <label>Item Name</label>
+        <input
+          type='text'
+          name="newItemName"
+          value={newItemName}
+          onChange={this.handleInputChange}
+        />
+        <label>Item Price</label>
+        <input
+          type='number'
+          name="newItemPrice"
+          value={newItemPrice}
+          onChange={this.handleInputChange}
+        />
+        <button onClick={this.handleAddItem}>Add Item</button>
+        <ChildComponent cartItems={this.state.cartItems} onRemoveItem={this.handleRemoveItem} />
       </div>
     );
   }
 }
 
-export default App
+export default App;
+
--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -3,14 +3,13 @@ import React from 'react';
 const ChildComponent = ({ cartItems, onRemoveItem }) => {
   return (
     <div className="child">
-      <ul>
-        {cartItems.map((item) => (
-          <li key={item.id}>
-            <span id="itemName">{item.name}</span> - <span id="itemPrice">${item.price}</span>
-            <button onClick={() => onRemoveItem(item.id)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+      {cartItems.map((item) => (
+        <div key={item.id}>
+          <span id="itemName">{item.name}</span>
+          <span id="itemPrice">{item.price}</span>
+          <button onClick={() => onRemoveItem(item.id)}>Remove</button>
+        </div>
+      ))}
     </div>
   );
 };
