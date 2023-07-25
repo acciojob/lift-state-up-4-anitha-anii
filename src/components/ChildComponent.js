90dbb654230cad723c0b647c423c05702de79ffc
anitha-anii <anithachowdary355@gmail.com>
2023-07-25 09:23:18 +0530

    'changes'

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 const ChildComponent = ({ cartItems, onRemoveItem }) => {
   return (
-   
-    <ul className="child li">
-         <h2>Child Component</h2>
-      {cartItems.map((item) => (
-        <li key={item.id}>
-          <span id="itemName">{item.name}</span>
-          <span id="itemPrice">{item.price}</span>
-          <button onClick={() => onRemoveItem(item.id)}>Remove</button>
-        </li>
-      ))}
-    </ul>
+    <div className="child">
+  <h2>Child Component</h2>
+  <ul>
+    {cartItems.map((item) => (
+      <li key={item.id} className='child li'>
+        <span id="itemName" contenteditable>{item.name}</span>
+        <span id="itemPrice">{item.price}</span>
+        <button onClick={() => onRemoveItem(item.id)}>Remove</button>
+      </li>
+    ))}
+  </ul>
+</div>
+
   );
 };
 
 export default ChildComponent;
+
