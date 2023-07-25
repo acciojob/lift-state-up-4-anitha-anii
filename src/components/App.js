f174437872adda78ceba68fe331e28a8e29bd925
anitha-anii <anithachowdary355@gmail.com>
2023-07-25 08:20:04 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,20 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cartItems: [
+      cartItems: [],
+      availableItems: [
         { id: 1, name: 'Item 1', price: 10 },
         { id: 2, name: 'Item 2', price: 20 },
         { id: 3, name: 'Item 3', price: 15 },
       ],
     };
   }
+
+  addToCart = (item) => {
+    this.setState((prevState) => ({
+      cartItems: [...prevState.cartItems, item],
+    }));
+  };
 
   removeItemFromCart = (itemId) => {
     this.setState((prevState) => ({
@@ -24,11 +31,26 @@ class App extends Component {
     return (
       <div className="parent">
         <h1>Shopping Cart</h1>
+        <div className="available-items">
+          <h2>Available Items</h2>
+          <ul>
+            {this.state.availableItems.map((item) => (
+              <li key={item.id}>
+                <span id="itemName">{item.name}</span> - <span id="itemPrice">${item.price}</span>
+                <button
+                  id="addToCartButton"
+                  onClick={() => this.addToCart(item)}
+                >
+                  Add to Cart
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
         <ChildComponent cartItems={this.state.cartItems} onRemoveItem={this.removeItemFromCart} />
       </div>
     );
   }
 }
-
 
 export default App
